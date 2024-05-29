74f390153caa7d28375c8ba88887d033afb00ce3
Bronson McKinley <mckinley@users.noreply.github.com>
2024-05-29 01:09:12 -0700

    custom domain for website

--- apps/website/webiny.application.ts
+++ apps/website/webiny.application.ts
@@ -1,5 +1,13 @@
 import { createWebsiteApp } from "@webiny/serverless-cms-aws";
 
 export default createWebsiteApp({
-    pulumiResourceNamePrefix: "wby-"
+    pulumiResourceNamePrefix: "wby-",
+    domains() {
+      return {
+        domains: ["mustacherides.com"],
+        sslSupportMethod: "sni-only",
+        acmCertificateArn:
+          "arn:aws:acm:us-east-1:445186575501:certificate/d361dba8-53c7-4b3a-938d-cf3be31132b0"
+      };
+    }
 });
