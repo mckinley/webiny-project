c0e08eb10f3a37fbd530bc20225d498f77d45294
Bronson McKinley <mckinley@users.noreply.github.com>
2024-05-29 16:45:13 -0700

    add wild card to website domain config

--- apps/website/webiny.application.ts
+++ apps/website/webiny.application.ts
@@ -4,7 +4,7 @@ export default createWebsiteApp({
     pulumiResourceNamePrefix: "wby-",
     domains() {
       return {
-        domains: ["mustacherides.com"],
+        domains: ["mustacherides.com", "*.mustacherides.com"],
         sslSupportMethod: "sni-only",
         acmCertificateArn:
           "arn:aws:acm:us-east-1:445186575501:certificate/d361dba8-53c7-4b3a-938d-cf3be31132b0"
