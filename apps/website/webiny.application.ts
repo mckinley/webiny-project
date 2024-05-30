b9e7cf8c5a35100e59d0ef251122915cebbc7b14
Bronson McKinley <mckinley@users.noreply.github.com>
2024-05-30 12:27:08 -0700

    only add custom domains to prod

--- apps/website/webiny.application.ts
+++ apps/website/webiny.application.ts
@@ -2,12 +2,15 @@ import { createWebsiteApp } from "@webiny/serverless-cms-aws";
 
 export default createWebsiteApp({
     pulumiResourceNamePrefix: "wby-",
-    domains() {
-      return {
-        domains: ["mustacherides.com", "*.mustacherides.com"],
-        sslSupportMethod: "sni-only",
-        acmCertificateArn:
-          "arn:aws:acm:us-east-1:992382537026:certificate/1da5b7ac-35c5-4cde-9f51-aaabbb838ff3"
-      };
+    domains({ params }) {
+        const { env } = params.run;
+        if (env === "prod") {
+            return {
+                domains: ["mustacherides.com", "*.mustacherides.com"],
+                sslSupportMethod: "sni-only",
+                acmCertificateArn:
+                    "arn:aws:acm:us-east-1:992382537026:certificate/1da5b7ac-35c5-4cde-9f51-aaabbb838ff3"
+            };
+        }
     }
 });
