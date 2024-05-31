2b6b69b18654f0caaacf62ab1664af5744d95d06
Bronson McKinley <mckinley@users.noreply.github.com>
2024-05-30 17:49:42 -0700

    update cert after creating new aws accounts

--- apps/website/webiny.application.ts
+++ apps/website/webiny.application.ts
@@ -9,7 +9,7 @@ export default createWebsiteApp({
                 domains: ["mustacherides.com", "*.mustacherides.com"],
                 sslSupportMethod: "sni-only",
                 acmCertificateArn:
-                    "arn:aws:acm:us-east-1:992382537026:certificate/1da5b7ac-35c5-4cde-9f51-aaabbb838ff3"
+                    "arn:aws:acm:us-east-1:905418069383:certificate/47a87675-7701-4b09-b830-8b0ac152aafc"
             };
         }
     }
